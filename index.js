4747877cde937d2be65620f7a14878d9dca912af
cnyaga66 <cnyga66@gmail.com>
2024-04-01 11:14:04 +0300

    tests

---
+++ index.js
@@ -0,0 +1,10 @@
+function shout(string) {
+    return string.toUpperCase();
+  }
+  function whisper(string) {
+    return string.toUpperCase().toLowerCase();
+  }
+  function sayHiToHeadphonedRoommate() {
+    return string.toUpperCase();
+  }
+  console.log(string === string.toUpperCase());
